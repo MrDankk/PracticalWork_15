f035a7e0e19bd42e90faefc5dcf434ed6d8108ae
Валера Безгребельный <Frostmols@gmail.com>
2024-09-16 01:50:37 +0300

    Добавьте файлы проекта.

---
+++ MethodLibrary/VoidMethod.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MethodLibrary
+{
+    public class VoidMethod
+    {
+
+        /// <summary>
+        /// Вывести в консоль информацию о потоке
+        /// </summary>
+        /// <param name="count"> Количество выполнений </param>
+        public static void Print(object count)
+        {
+            Random random = new Random();
+
+            Console.WriteLine($"Поток {Task.CurrentId} Начал работу.");
+
+            int cyclesCount = (int)count;
+
+            for (int i = 0; i < cyclesCount; i++)
+            {
+                Console.WriteLine($"Поток {Task.CurrentId} - выполнился {i + 1} раз(а)");
+                Thread.Sleep(random.Next(100,250));
+            }
+
+            Console.WriteLine($"Поток {Task.CurrentId} Закончил работу.");
+        }
+    }
+}
---
+++ PracticalWork_15/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MethodLibrary;
+
+namespace PracticalWork_15
+{
+    internal class Program
+    {
+        static int TaskCount;
+        static int CyclesCount;
+
+        static void Main(string[] args)
+        {
+            MenuSelection();
+        }
+
+        /// <summary>
+        /// Управленние главным меню
+        /// </summary>
+        static void MenuSelection()
+        {
+            bool menuActive = true;
+
+            while (menuActive)
+            {
+                Console.Clear();
+                PrintMenuSelection();
+                int input = Input();
+
+                switch (input)
+                {
+                    case 1:
+                        Console.WriteLine("Введите количество потоков");
+                        TaskCount = Input();
+                        break;
+
+                    case 2:
+                        if(TaskCount == 0) TaskCount = 1;
+
+                        Console.WriteLine("Введите количество выполнений");
+                        CyclesCount = Input();
+                        ProgramStart();
+                        break;
+
+                    case 0:
+                        menuActive = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Такого пункта не существует");
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Вывод в консоль меню выбора
+        /// </summary>
+        static void PrintMenuSelection()
+        {
+            Console.WriteLine("1 - Количество потоков");
+            Console.WriteLine("2 - Запусить выполнение");
+            Console.WriteLine("0 - Выход");
+        }
+
+        /// <summary>
+        /// Запуск программы
+        /// </summary>
+        static void ProgramStart()
+        {
+            Task[] tasks = new Task[TaskCount];
+
+            for (int i = 0; i < TaskCount; i++)
+            {
+                tasks[i] = Task.Factory.StartNew(VoidMethod.Print,CyclesCount);
+            }
+
+            Task.WaitAll(tasks);
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Проверка ввода
+        /// </summary>
+        /// <returns></returns>
+        static int Input()
+        {
+            while(true)
+            {
+                int result;
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out result))
+                {
+                    return result;
+                }
+                else
+                {
+                    Console.WriteLine("Некоректный ввод");
+                }
+            }
+        }
+    }
+}
